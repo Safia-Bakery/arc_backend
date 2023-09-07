99efe37577b9fae7c66a0d83db76e9b8f0f997d7
Gayratbek <gayratbekakhmedov@gmail.com>
2023-09-07 14:17:41 +0500

    sdf

--- microservices.py
+++ microservices.py
@@ -232,7 +232,6 @@ def send_document_iiko(key,data):
     if data.request.category.sphere_status==2:
         pass
     response = requests.post(f"{BASE_URL}/resto/api/documents/import/outgoingInvoice?key={key}",data=xml_data,headers=headers)
-    print(response.content)
     
     return True
     
