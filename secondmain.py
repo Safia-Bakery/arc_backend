00394cde07498e4142c07e1b9b59f16033da5fa5
Gayratbek <gayratbekakhmedov@gmail.com>
2023-09-08 14:37:27 +0500

    sd

--- secondmain.py
+++ secondmain.py
@@ -266,9 +266,9 @@ async def get_category(files:list[UploadFile],category_id:int,fillial_id:UUID,de
                 for i in responserq.file:
                     keyboard.append({'text':'Посмотреть фото/видео',"url":f"{BASE_URL}{i.url}"})
             if responserq.category.sphere_status==1 and responserq.category.department==1:
-                sendtotelegram(bot_token=bot_token,chat_id='-1920671327',message_text=text,keyboard=keyboard)
+                sendtotelegram(bot_token=bot_token,chat_id='-1001920671327',message_text=text,keyboard=keyboard)
             if responserq.category.sphere_status==1 and responserq.category.department==2:
-                sendtotelegram(bot_token=bot_token,chat_id='-1831677963',message_text=text,keyboard=keyboard)
+                sendtotelegram(bot_token=bot_token,chat_id='-1001831677963',message_text=text,keyboard=keyboard)
             return {'success':True,'message':'everything is saved'}
         #except:
         #    raise HTTPException(
