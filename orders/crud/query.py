dd24829acc47e457c723f78664180b8c08fef140
Gayratbek <gayratbekakhmedov@gmail.com>
2023-12-01 16:49:13 +0500

    edti data

--- orders/crud/query.py
+++ orders/crud/query.py
@@ -11,7 +11,7 @@ from sqlalchemy import or_,and_,Date,cast,Integer
 
 
 def marketing_table(db:Session,timer,created_at,finished_at):
-    total = db.query(models.Category.name,func.count(models.Requests.id),func.cast(func.avg(func.extract('epoch', models.Requests.finished_at -models.Requests.started_at)) /timer, Integer)).join(models.Requests).filter(models.Requests.status==3,models.Category.department==3,models.Requests.created_at.between(created_at,finished_at)).group_by(models.Category.name).all()
+    total = db.query(models.Category.sub_id,func.count(models.Requests.id),func.cast(func.avg(func.extract('epoch', models.Requests.finished_at -models.Requests.started_at)) /timer, Integer)).join(models.Requests).filter(models.Requests.status==3,models.Category.department==3,models.Requests.created_at.between(created_at,finished_at)).group_by(models.Category.sub_id).all()
     dict_data = {}
     for i in total:
         dict_data[i[0]] = [i[1],i[2]]
@@ -23,17 +23,17 @@ def marketing_pie(db:Session,created_at,finished_at):
     db.query(
         func.count(models.Requests.id).label('total_count'),
         func.jsonb_build_object(
-            'category_name', models.Category.name,
+            'category_sub_id', models.Category.sub_id,
             'percent', func.cast(func.count(models.Requests.id) * 100.0 / func.nullif(func.sum(func.count(models.Requests.id)).over(), 0), Integer)
         ).label('category_percent')
     )
     .join(models.Requests)
     .filter(models.Requests.status == 3, models.Category.department == 3,models.Requests.created_at.between(created_at,finished_at))
-    .group_by(models.Category.name)
+    .group_by(models.Category.sub_id)
     #.group_by(models.Requests.status)
     .all()
     )
     dict_data = {}
     for i in percentages:
-        dict_data[i[1]['category_name']] = [i[0],i[1]['percent']]
+        dict_data[i[1]['category_sub_id']] = [i[0],i[1]['percent']]
     return dict_data
