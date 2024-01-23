1b251aeb1f132dacf183242c15dbe4cd3ffa5b51
Gayratbek <gayratbekakhmedov@gmail.com>
2024-01-23 21:52:15 +0500

    edit dta

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def startup_event():
 @app.on_event("startup")
 def meal_messages():
     scheduler = BackgroundScheduler()
-    trigger  = CronTrigger(hour=21, minute=52, second=00,timezone=timezonetash)
+    trigger  = CronTrigger(hour=21, minute=53, second=00,timezone=timezonetash)
     scheduler.add_job(meal_pushes, trigger=trigger,args=[next(get_db())])
     scheduler.start()
 
