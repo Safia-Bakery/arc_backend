7c80d981eb954cdccc91fad9096489a4a2498e46
Gayratbek <gayratbekakhmedov@gmail.com>
2024-01-31 18:09:19 +0500

    edit

--- statisquery.py
+++ statisquery.py
@@ -470,7 +470,7 @@ def safia_eats(db:Session,request_data):
 
 
 def marketing_stats_v2(db:Session,started_at, finished_at,department,sphere_status,sub_id,timer=60):
-    categories = db.query(models.Category)
+    categories = db.query(models.Category).filter(models.Category.status==1)
     if sphere_status is not None:
         categories = categories.filter(models.Category.sphere_status==sphere_status)
     if sub_id is not None:
