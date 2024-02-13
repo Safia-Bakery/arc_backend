f9e3a58d166d29e28e47b4a8d30bc2269ce8e247
Gayratbek <gayratbekakhmedov@gmail.com>
2024-02-13 22:11:54 +0500

    sd

--- statisquery.py
+++ statisquery.py
@@ -642,20 +642,19 @@ def inventory_stats(db:Session,started_at,finished_at,department,timer=60):
             models.Tools.ftime.is_not(None)).filter(
             models.Requests.status.in_([0,1,2,3])).count()
         
-        on_time_requests = db.query(models.Expanditure).join(models.Requests).join(models.Category).join(models.Tools).filter(
-         (models.Requests.finished_at - models.Requests.started_at) <= ftime_timedelta,
-        ).filter(models.Tools.parentid==parent_id.parentid).filter(
+        on_time_requests = db.query(models.Expanditure).join(models.Requests).join(models.Category).join(models.Tools).filter(models.Tools.parentid==parent_id.parentid).filter(
             models.Category.department==department).filter(
             models.Tools.ftime.is_not(None)).filter(
-            models.Requests.status.in_([3])).count()
+            models.Requests.status.in_([3])).filter(
+        (models.Requests.finished_at - models.Requests.started_at) <= ftime_timedelta,).count()
+         
 
 
-        not_finishedon_time =db.query(models.Expanditure).join(models.Requests).join(models.Tools).join(models.Category).filter(
-       (models.Requests.finished_at - models.Requests.started_at) >ftime_timedelta,
-        ).filter(models.Tools.parentid==parent_id.parentid).filter(
+        not_finishedon_time =db.query(models.Expanditure).join(models.Requests).join(models.Tools).join(models.Category).filter(models.Tools.parentid==parent_id.parentid).filter(
             models.Category.department==department).filter(
             models.Tools.ftime.is_not(None)).filter(
-            models.Requests.status.in_([3])).count()
+            models.Requests.status.in_([3])).filter(
+       (models.Requests.finished_at - models.Requests.started_at) >ftime_timedelta,).count()
 
         # not_finishedon_time =db.query(models.Expanditure).join(models.Requests).join(models.Tools).join(models.Category).filter(
         # models.Requests.status == 3,
