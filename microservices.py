df63840521f99906b7ab515bddd369cda4dc6448
Gayratbek <gayratbekakhmedov@gmail.com>
2024-03-01 10:01:34 +0500

    sd

--- microservices.py
+++ microservices.py
@@ -487,15 +487,19 @@ def Excell_generate_it(data):
 
 
         if row.update_time:
-            reshen_time = dict(row.update_time).get('6').strftime("%d.%m.%Y %H:%M:%S")
-
+            reshen_time = dict(row.update_time).get('6')
+            if reshen_time:
+                reshen_time = reshen_time.strftime("%d.%m.%Y %H:%M:%S") 
+            
             inserting_data['Дата решения'].append(reshen_time)
         else:
             inserting_data['Дата решения'].append("")
         
         if row.status==6:
             if row.update_time:
-                cancel_time = dict(row.update_time).get('6').strftime("%d.%m.%Y %H:%M:%S")
+                cancel_time = dict(row.update_time).get('6')
+                if cancel_time:
+                    cancel_time = cancel_time.strftime("%d.%m.%Y %H:%M:%S")
                 inserting_data['Дата отмены'].append(cancel_time)
             else:
                 inserting_data['Дата отмены'].append("")
