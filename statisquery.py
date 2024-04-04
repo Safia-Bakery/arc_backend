16c028634905b61a308c9288d946e7ed458dc819
Gayratbek <gayratbekakhmedov@gmail.com>
2024-04-04 17:44:59 +0500

    stats change and ureven service

--- statisquery.py
+++ statisquery.py
@@ -468,6 +468,7 @@ def avg_time_finishing(db:Session,department,sphere_status,sub_id,timer=60 ):
         ).join(models.Category).filter(
             models.Requests.status == 3,
             models.Category.department == department)
+    query = query.distinct(models.Comments.request_id)
     if sphere_status is not None:
         total = total.filter(models.Category.sphere_status == sphere_status) 
     if sub_id is not None:
