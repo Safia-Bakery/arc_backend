60a2d223bd2593cb0577559e2f404fa8b57ec1bd
Gayratbek <gayratbekakhmedov@gmail.com>
2024-09-06 10:31:04 +0500

    add calendars

--- app/crud/users.py
+++ app/crud/users.py
@@ -7,7 +7,7 @@ from sqlalchemy.sql import func
 from datetime import datetime,timedelta
 from sqlalchemy import or_, and_, Date, cast,String
 from uuid import UUID
-from app.models.users_model import Users
+from models import Users
 
 
 def get_user_by_username(db:Session,username:str):
