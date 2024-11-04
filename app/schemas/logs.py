60f6dca11a8e6c2a4fb9cca33a47163f487a877c
Gayratbek <gayratbekakhmedov@gmail.com>
2024-11-04 10:12:31 +0500

    add log

--- app/schemas/logs.py
+++ app/schemas/logs.py
@@ -3,12 +3,10 @@ from typing import Optional, Dict
 from datetime import datetime
 from .fillials import GetFillialChild, GetParentFill
 from .tools import GetTool
-from .requests import GetOneRequest
 from .users import UserFullBack,UserGetJustNames
 
 
 class CreateLogs(BaseModel):
-    request: GetOneRequest
     user: UserFullBack
     created_at: Optional[datetime]
     status: Optional[int] = None
