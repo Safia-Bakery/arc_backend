e0f8e507e49f0b95571dd7b725f492fc435a7a3f
Gayratbek <gayratbekakhmedov@gmail.com>
2024-11-05 10:17:55 +0500

    file upload multiple files;

--- app/routes/v2/endpoints/files.py
+++ app/routes/v2/endpoints/files.py
@@ -15,7 +15,7 @@ async def read_files(
     db: Session = Depends(get_db),
     current_user: GetUserFullData = Depends(get_current_user),
 ):
-    file_path = []
+    file_paths = []
     for file in files:
         file_path = f"files/{generate_random_string(10)}{file.filename}"
         with open(file_path, "wb") as buffer:
@@ -24,6 +24,6 @@ async def read_files(
                 if not chunk:
                     break
                 buffer.write(chunk)
-        file_path.append(file_path)
-    return {"files": file_path}
+        file_paths.append(file_path)
+    return {"files": file_paths}
 
