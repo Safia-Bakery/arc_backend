1a2086f1893192c7b924329100b5166b7c801454
Gayratbek <gayratbekakhmedov@gmail.com>
2024-11-22 11:17:32 +0500

    add

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ ALGORITHM = os.environ.get("ALGORITHM")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 
-origins = ["*"]
+origins = ["https://service.safiabakery.uz"]
 
 reuseable_oauth = OAuth2PasswordBearer(tokenUrl="/login", scheme_name="JWT")
 # database connection
