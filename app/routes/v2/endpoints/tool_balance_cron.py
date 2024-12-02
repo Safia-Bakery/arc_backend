b90428997fcd6ac089df0cc895c04167c40b9499
Data Engineer <dataeng27@gmail.com>
2024-12-02 12:06:16 +0500

    disabled tool balance cron job

--- app/routes/v2/endpoints/tool_balance_cron.py
+++ app/routes/v2/endpoints/tool_balance_cron.py
@@ -15,22 +15,22 @@ tool_balance_cron_router = APIRouter()
 timezonetash = pytz.timezone('Asia/Tashkent')
 
 
-def product_balance_job(db: Session):
-    api_route = ApiRoutes()
-    department_list = get_all_departments(db=db)
-    for department in department_list:
-        product_balance_list = api_route.get_tool_balance(department.id)
-        create_update_tool_balance(db, product_balance_list, department.id)
-    del product_balance_list
-    del department_list
+# def product_balance_job(db: Session):
+#     api_route = ApiRoutes()
+#     department_list = get_all_departments(db=db)
+#     for department in department_list:
+#         product_balance_list = api_route.get_tool_balance(department.id)
+#         create_update_tool_balance(db, product_balance_list, department.id)
+#     del product_balance_list
+#     del department_list
+#
+#     return True
 
-    return True
 
-
-@tool_balance_cron_router.on_event("startup")
-def startup_event():
-    scheduler = BackgroundScheduler()
-    trigger = CronTrigger(hour=16, minute=00, second=00, timezone=timezonetash)  # Set the desired time for the function to run (here, 12:00 PM)
-    scheduler.add_job(product_balance_job, trigger=trigger, args=[next(get_db())])
-    scheduler.start()
+# @tool_balance_cron_router.on_event("startup")
+# def startup_event():
+#     scheduler = BackgroundScheduler()
+#     trigger = CronTrigger(hour=16, minute=00, second=00, timezone=timezonetash)  # Set the desired time for the function to run (here, 12:00 PM)
+#     scheduler.add_job(product_balance_job, trigger=trigger, args=[next(get_db())])
+#     scheduler.start()
 
