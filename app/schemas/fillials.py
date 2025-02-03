5feadd6852ed9e9f153fcfa9b628640e777a8bf9
Gayratbek <gayratbekakhmedov@gmail.com>
2025-02-03 12:19:26 +0500

    add manager to requests

--- app/schemas/fillials.py
+++ app/schemas/fillials.py
@@ -5,6 +5,7 @@ from datetime import datetime, time
 from fastapi import Form
 from uuid import UUID
 from orders.schema import schema_router
+from app.schemas.manager_branchs import getManagers
 
 
 class GetParentFill(BaseModel):
@@ -19,6 +20,7 @@ class GetFillialChild(BaseModel):
     name: str
     origin: int
     parentfillial: Optional[GetParentFill] = None
+    manager:Optional[getManagers]=None
 
     class Config:
         orm_mode = True
